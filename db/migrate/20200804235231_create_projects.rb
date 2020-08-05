c12410a2688c7d9f55c3d8da7a4267b7c90a73f8
Argonity <maurice.argoetti@gmail.com>
2020-08-04 20:23:47 -0400

    added attributes to Project table

--- db/migrate/20200804235231_create_projects.rb
+++ db/migrate/20200804235231_create_projects.rb
@@ -2,6 +2,26 @@ class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
       t.string :street_address
+      t.string :city
+      t.string :state
+      t.integer :zip_code
+      t.string :type
+      t.integer :year_built
+      t.integer :square_footage
+      t.boolean :pool
+      t.boolean :garage
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.integer :purchase_price
+      t.integer :sale_price
+      t.integer :renovation_budget
+      t.integer :profit
+      t.integer :current_status
+      t.string :nickname
+      t.string :description
+
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :contractor, foreign_key: true
 
       t.timestamps
     end

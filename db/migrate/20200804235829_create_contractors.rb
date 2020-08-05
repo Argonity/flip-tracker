148b5412577763f15fca926ca62b8f69e8a1efe2
Argonity <maurice.argoetti@gmail.com>
2020-08-04 20:27:38 -0400

    added attributes to Contractor table

--- db/migrate/20200804235829_create_contractors.rb
+++ db/migrate/20200804235829_create_contractors.rb
@@ -2,6 +2,7 @@ class CreateContractors < ActiveRecord::Migration[6.0]
   def change
     create_table :contractors do |t|
       t.string :name
+      t.string :phone_number
 
       t.timestamps
     end

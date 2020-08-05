6e6db1997154423fc586023c2338188327018dc3
Argonity <maurice.argoetti@gmail.com>
2020-08-05 19:29:25 -0400

    modified Project table attribute

--- db/migrate/20200804235231_create_projects.rb
+++ db/migrate/20200804235231_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-      t.string :street_address
+      t.string :street
       t.string :city
       t.string :state
       t.string :zip

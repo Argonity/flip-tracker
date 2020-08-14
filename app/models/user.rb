e9da5921f48866437ac6786242ca5de557db9753
Argonity <maurice.argoetti@gmail.com>
2020-08-13 20:09:17 -0400

    added username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :projects
   has_many :contractors, through: :projects
+
+  validates :username, uniqueness: true, presence: true
+
   has_secure_password
 end

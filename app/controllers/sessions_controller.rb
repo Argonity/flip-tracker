17e175e9987769c94e5661690fdf1975e887944b
Argonity <maurice.argoetti@gmail.com>
2020-08-18 20:41:59 -0400

    coded create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,17 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    #find the user in database
+    @user = User.find_by(username: params[:user][:username])
+    #does the user exist and is their password correct?
+    if @user.try(:authenticate, params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:error] = "Username or password incorrect"
+      redirect_to login_path
+    end
+  end
+
 end

bfcd671e0cd4a9609bbcea05485e6c19a538c0b0
Argonity <maurice.argoetti@gmail.com>
2020-09-02 20:43:13 -0400

    added build method to new. coded create and params methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,23 @@ class ProjectsController < ApplicationController
 
 def new
   @project = Project.new
+  @project.build_contractor
+end
+
+def create
+  @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+end
+
+private
+
+def project_params
+  params.require(:project).permit(:nickname, :description, :contractor_id,
+  contractor_attributes: [:name])
 end
 
 end
